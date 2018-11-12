0cd29ff6a74892893326d8c6845b1ed9f3ca90b1
devtuananh <tran.tuan.anhb@framgia.com>
2018-11-12 15:00:28 +0700

    Admin manage category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :songs, dependent: :destroy
+  has_many :songs
+  validates :name, presence: true
+  validates :info, presence: true
+  scope :select_fields, ->{includes(:songs).select :id, :name, :info, :created_at}
+  scope :by_lastest,->{order created_at: :desc}
   scope :by_name_cate, -> {order :name}
 end
